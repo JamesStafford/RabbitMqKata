aa8b344356aac5da40f074c24efdce9379ea08bf
jstafford <jwstafford1991@gmail.com>
2025-06-14 08:42:57 -0500

    Make `MessageSubscriber` static and implement a retry mechanism for message subscription

--- RabbitMQKata/src/Subscriber/MessageSubscriber.cs
+++ RabbitMQKata/src/Subscriber/MessageSubscriber.cs
@@ -3,23 +3,25 @@ using RabbitMQKata.Publisher;
 
 namespace RabbitMQKata.Subscriber;
 
-public class MessageSubscriber
+public static class MessageSubscriber
 {
     public static async Task SubscribeToMessages(string username, string password)
     {
         var connectionString = $"host=rabbit;username={username};password={password}";
     
         using var bus = RabbitHutch.CreateBus(connectionString);
-    
-        await bus.PubSub.SubscribeAsync<TextMessage>(
-            subscriptionId: "my_subscription", // unique subscription ID
-            message => {
-                Console.WriteLine($"Received message: {message.Content}");
-                return Task.CompletedTask;
-            });
-        
-        // Keep the subscription alive
-        Console.WriteLine("Waiting for messages. Press any key to exit.");
-        Console.ReadKey();
+
+        while (true)
+        {
+            await bus.PubSub.SubscribeAsync<TextMessage>(
+                subscriptionId: "my_subscription", // unique subscription ID
+                message =>
+                {
+                    Console.WriteLine($"Received message: {message.Content}");
+                    return Task.CompletedTask;
+                });
+            
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
     }
 }
